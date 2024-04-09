aca96d1447a293727a0df8876e7bc8da6b615858
ikhsangym <114869784+Ikhsangym@users.noreply.github.com>
2024-04-09 09:52:11 +0800

    Laporan Praktikum

---
+++ Modul1/PRAK101.php
@@ -0,0 +1,6 @@
+<?php
+    echo "Hello World in PHP <br>";
+    echo "Nama Praktikan : Ikhsan Gymnastiar <br>";
+    echo "NIM : 2210817310005";
+?>
+
---
+++ Modul1/PRAK102.php
@@ -0,0 +1,5 @@
+<?php
+    $jari_jari_bola = 4.2;
+    $volume = 4/3 * 22/7 * $jari_jari_bola * $jari_jari_bola * $jari_jari_bola;
+    echo number_format($volume, 3, ',')." m3";
+?>
